10e03e345d3ab4a72ad753b190c649b887958be4
Chris Bogart <cbogart@users.noreply.github.com>
2019-04-19 17:43:13 -0400

    Allow changing API endpoint
    The original service is gone, but the code is available, so one might set up one's own endpoint

--- mercury_parser/client.py
+++ mercury_parser/client.py
@@ -6,15 +6,16 @@ API_ENDPOINT = 'https://mercury.postlight.com/parser?url={}'
 
 
 class MercuryParser(object):
-    def __init__(self, api_key):
+    def __init__(self, api_key, api_endpoint=API_ENDPOINT):
         self.api_key = api_key
+        self.api_endpoint = api_endpoint
         self.headers = {'x-api-key': self.api_key}
 
     def parse_article(self, article_url):
         """
         Parse article URL
         """
-        response = requests.get(API_ENDPOINT.format(article_url), headers=self.headers)
+        response = requests.get(self.api_endpoint.format(article_url), headers=self.headers)
         return response
 
     def parse_multiple_articles(self, *urls):
@@ -25,7 +26,7 @@ class MercuryParser(object):
         parsed_articles = {}
         session = requests.Session()
         for url in urls:
-            response = session.get(API_ENDPOINT.format(url), headers=self.headers)
+            response = session.get(self.api_endpoint.format(url), headers=self.headers)
             parsed_articles[url] = response.json()
 
         response = json.loads(json.dumps(parsed_articles))
